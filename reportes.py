7e498890a86a75aa5b338b986264ca717437493c
JimmyGarciaf <jimmygarcia470@gmail.com>
2024-04-21 19:44:34 -0600

    Update

--- reportes.py
+++ reportes.py
@@ -3,3 +3,34 @@ from email.mime.multipart import MIMEMultipart # importa la clase MIMEMultipart 
 from email.mime.text import MIMEText # Similar al punto anterior se utiliza para crear partes de texto para el mensaje de correo electrónico.
 import os #Esto importa el módulo os, que proporciona funciones para interactuar con el sistema operativo, como leer y escribir archivos, manipular rutas de archivo, y otras operaciones relacionadas con el sistema de archivos.
 
+# Esta función se encarga de configurar y enviar un correo electrónico con los parámetros especificados.
+
+def enviar_correo(destinatario, asunto, cuerpo, adjunto=None):
+    # Configurar servidor SMTP
+    servidor_smtp = 'corporacion@mcc.hn'
+    puerto_smtp = 587
+    usuario_smtp = 'jgarcia@mcc.hn'
+    contraseña_smtp = 'Garcia.2024'
+
+    # Crear objeto mensaje
+    mensaje = MIMEMultipart()
+    mensaje['From'] = usuario_smtp
+    mensaje['To'] = destinatario
+    mensaje['Subject'] = asunto
+
+    # Adjuntar cuerpo del correo
+    mensaje.attach(MIMEText(cuerpo, 'plain'))
+
+    # Adjuntar archivo si existe
+    if adjunto:
+        adjunto_mime = MIMEText(open(adjunto, 'rb').read(), 'plain')
+        adjunto_mime.add_header('Content-Disposition', f'attachment; filename={os.path.basename(adjunto)}')
+        mensaje.attach(adjunto_mime)
+        
+     # Iniciar conexión SMTP y enviar correo
+    with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
+        servidor.starttls()
+        servidor.login(usuario_smtp, contraseña_smtp)
+        servidor.send_message(mensaje)
+
+    print("Correo enviado con éxito!")
